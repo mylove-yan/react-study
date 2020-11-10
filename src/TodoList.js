8ac3761190b586726f845aafdc9719b18aca0a30
马誌浩 <mazh@emrubik.com>
2020-11-10 08:38:00 +0800

    #todoList 学习

---
+++ src/TodoList.js
@@ -0,0 +1,53 @@
+import React, {Component, Fragment} from 'react'
+
+class TodoList extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            inputValue: '',
+            list: []
+        }
+    }
+
+    render() {
+        return (
+            <Fragment>
+                <div>
+                    <input value={this.state.inputValue} onChange={this.handleInputChange.bind(this)}/>
+                    <button onClick={this.handleBtnClick}>
+                        提交
+                    </button>
+                </div>
+                <ul>
+                    {this.state.list.map((value, index)=>{
+                        return <li key={index} onClick={this.handleDelete}>{value}</li>
+                    })}
+                </ul>
+            </Fragment>
+        );
+    }
+
+    handleInputChange(e) {
+        this.setState({
+            inputValue: e.target.value
+        })
+    }
+
+    handleBtnClick = () => {
+        this.setState({
+            list: [...this.state.list, this.state.inputValue],
+            inputValue:''
+        })
+    }
+    handleDelete = (index)=>{
+        let list = [...this.state.list];
+        list.splice(index,1);
+        this.setState({
+            list
+        });
+    }
+}
+
+export default TodoList;
