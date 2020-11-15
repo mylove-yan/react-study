4fe122015eaa837bccd84889332877df440e9674
Administrator <jl20103962@163.com>
2020-11-15 20:34:31 +0800

    #开始简书项目搭建

---
+++ src/common/header/index.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+import {HeaderWrapper} from './style';
+
+class Header extends Component {
+    render() {
+        return (
+            <HeaderWrapper>
+                header
+            </HeaderWrapper>
+        )
+    }
+}
+export default Header;
