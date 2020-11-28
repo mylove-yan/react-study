3c1063796cdfc2cc7c898241f1f3904efd7f1a45
Administrator <jl20103962@163.com>
2020-11-28 17:18:18 +0800

    #去哪网项目DEMO

--- src/todoAntd/store/actionTypes.js
+++
@@ -1,5 +0,0 @@
-export  const CHANGE_INPUT_VALUE =  'change_input_value';
-export  const HANDLE_BTN_CLICK =  'handle_btn_click';
-export  const LIST_ITEM_DELETE =  'list_item_delete';
-export  const INIT_LIST_DATA =  'init_list_data';
-
